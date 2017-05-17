334c02923a7d24d2234372c1e3464969721cb6fe
Eric Jacobson <contact@efjacobson.com>
2017-05-17 12:11:00 -0700

    refactor according to feedback

--- src/wbgpt.js
+++ src/wbgpt.js
@@ -1,252 +1,32 @@
 // todo: eslint faceoff(nicolasCage);
 // probably just move all this to index.js (this is the "main" entry point)
-const slots = {}; // todo: make this a map
-let displayProvider = googletag;
+const slots = new Map(); // todo: make this a map
+window.displayProvider = googletag;
 let slotIndex = 1;
 let prefix = 'wbgpt-';
 
 function setPrefix(newPrefix) {
   prefix = newPrefix;
+  return this;
 }
 
 // todo: rename to Slot, move to Slot.js (export default class Slot ...)
 // todo: proper jsdocs (all files)
 // todo: make fluent interface, i.e. return  this from all methods unless returning a value already.
-class BaseSlot {
 
-  // todo: add constructor, create gpt slot in constructor.  call it "this.gptSlot".
-
-  /*** custom functions ***/
-
-  /**
-   * displays slot using displayProvider
-   */
-  display() {
-    displayProvider.display(this.config.id);
-    return this; // example "fluent interface"
-  }
-
-  /**
-   * refreshes slot using displayProvider
-   */
-  refresh() {
-    displayProvider.pubads().refresh([this.slot]);
-    return this;
-  }
-
-  /**
-   * Returns config (beefcake details or describe the shape)
-   *
-   * @returns {Object}
-   */
-  getConfig() {
-    return this.config;
-  }
-
-  /**
-   * @link https://developers.google.com/doubleclick-gpt/reference#googletag.Slot_addService
-   */
-  addService(service) {
-    this.slot.addService(service);
-  }
-
-  /**
-   * @link https://developers.google.com/doubleclick-gpt/reference#googletag.Slot_clearCategoryExclusions
-   */
-  clearCategoryExclusions() {
-    this.slot.clearCategoryExclusions();
-  }
-
-  /**
-   * @link https://developers.google.com/doubleclick-gpt/reference#googletag.Slot_clearTargeting
-   */
-  clearTargeting(opt_key) {
-    this.slot.clearTargeting(opt_key);
-  }
-
-  /**
-   * @link https://developers.google.com/doubleclick-gpt/reference#googletag.Slot_defineSizeMapping
-   */
-  defineSizeMapping(mapping) {
-    this.slot.defineSizeMapping(mapping);
-  }
-
-  /**
-   * @link https://developers.google.com/doubleclick-gpt/reference#googletag.Slot_get
-   */
-  get(key) {
-    this.slot.get(key);
-  }
-
-  /**
-   * @link https://developers.google.com/doubleclick-gpt/reference#googletag.Slot_getAdUnitPath
-   */
-  getAdUnitPath() {
-    this.slot.getAdUnitPath();
-  }
-
-  /**
-   * @link https://developers.google.com/doubleclick-gpt/reference#googletag.Slot_getAttributeKeys
-   */
-  getAttributeKeys() {
-    this.slot.getAttributeKeys();
-  }
-
-  /**
-   * @link https://developers.google.com/doubleclick-gpt/reference#googletag.Slot_getCategoryExclusions
-   */
-  getCategoryExclusions() {
-    this.slot.getCategoryExclusions();
-  }
-
-  /**
-   * @link https://developers.google.com/doubleclick-gpt/reference#googletag.Slot_getResponseInformation
-   */
-  getResponseInformation() {
-    this.slot.getResponseInformation();
-  }
-
-  /**
-   * @link https://developers.google.com/doubleclick-gpt/reference#googletag.Slot_getSlotElementId
-   */
-  getSlotElementId() {
-    this.slot.getSlotElementId();
-  }
-
-  /**
-   * @link https://developers.google.com/doubleclick-gpt/reference#googletag.Slot_getTargeting
-   */
-  getTargeting(key) {
-    this.slot.getTargeting(key);
-  }
-
-  /**
-   * @link https://developers.google.com/doubleclick-gpt/reference#googletag.Slot_getTargetingKeys
-   */
-  getTargetingKeys() {
-    this.slot.getTargetingKeys();
-  }
-
-  /**
-   * @link https://developers.google.com/doubleclick-gpt/reference#googletag.Slot_set
-   */
-  set(key, value) {
-    this.slot.set(key, value);
-  }
-
-  /**
-   * @link https://developers.google.com/doubleclick-gpt/reference#googletag.Slot_setCategoryExclusion
-   */
-  setCategoryExclusion(categoryExclusion) {
-    this.slot.setCategoryExclusion(categoryExclusion);
-  }
-
-  /**
-   * @link https://developers.google.com/doubleclick-gpt/reference#googletag.Slot_setClickUrl
-   */
-  setClickUrl(value) {
-    this.slot.setClickUrl(value);
-  }
-
-  /**
-   * @link https://developers.google.com/doubleclick-gpt/reference#googletag.Slot_setCollapseEmptyDiv
-   */
-  setCollapseEmptyDiv(collapse, opt_collapseBeforeAdFetch) {
-    this.slot.setCollapseEmptyDiv(collapse, opt_collapseBeforeAdFetch);
-  }
-
-  /**
-   * @link https://developers.google.com/doubleclick-gpt/reference#googletag.Slot_setForceSafeFrame
-   */
-  setForceSafeFrame(forceSafeFrame) {
-    this.slot.setForceSafeFrame(forceSafeFrame);
-  }
-
-  /**
-   * @link https://developers.google.com/doubleclick-gpt/reference#googletag.Slot_setSafeFrameConfig
-   */
-  setSafeFrameConfig(config) {
-    this.slot.setSafeFrameConfig(config);
-  }
-
-  /**
-   * @link https://developers.google.com/doubleclick-gpt/reference#googletag.Slot_setTargeting
-   */
-  setTargeting(key, value) {
-    this.slot.setTargeting(key, value);
-  }
-}
-
-/**
- * out of page slot class
- * @link https://developers.google.com/doubleclick-gpt/reference#googletag.defineOutOfPageSlot
- * @param {string} unit - unit path ie '/1234567/sports'
- */
-// todo: move to OutOfPageSlot.js, export default class
-class OutOfPageSlot extends BaseSlot {
-  constructor(unit) {
-    // todo: move this whole init to "Slot" aka "BaseSlot".
-
-
-    const id = `${prefix}-${slotIndex}`;
-    super(); // past unit
-    this.slot = googletag.defineOutOfPageSlot(unit, id);
-    this.slot.setTargeting('tile', slotIndex);
-    this.slot.addService(googletag.pubads());
-    slotIndex += 1;
-
-    this.config = {
-      id: id, // call it domId instead or is this obvious enough?
-      isInterstitial: true,
-      unit: unit // todo: is this was google calls it?  or is it unit name?
-    };
-    slots[id] = this; // remove, see "createBlah" functions...
-  }
-}
-
-/**
- * base slot class
- * @link https://developers.google.com/doubleclick-gpt/reference#googletag.defineSlot
- * @param {string} unit - unit path ie '/1234567/sports'
- * @param {Array} sizeMap - default size ie [728, 90]
- */
-class Slot extends BaseSlot {
-  constructor(unit, sizeMap) {
-    // should be in "Slot" constructor
-    const id = `${prefix}-${slotIndex}`;
-    super();
-    this.slot = googletag.defineSlot(unit, sizeMap, id);
-    this.slot.setTargeting('tile', slotIndex);
-    this.slot.addService(googletag.pubads());
-    slotIndex += 1;
-
-    this.config = {
-      id: id,
-      sizeMap: sizeMap,
-      unit: unit
-    };
-    slots[id] = this; // remove
-  }
-}
-
-/**
- * leaderboard slot class
- * @link https://developers.google.com/doubleclick-gpt/reference#googletag.Slot_defineSizeMapping
- * @link https://support.google.com/dfp_premium/answer/1100453?hl=en
- */
-// todo: make "factory" function instead of class...
-// example:
+import Slot from './Slot.js';
+import OutOfPageSlot from './OutOfPageSlot.js';
 
 /**
  * @param {string} unit
  *
  * @returns {Slot}
  */
-function createSlot(unit, id = null) {
-  const domId = id || 'makeoneupwithslotindex?';
-  const slot = new Slot('someunit', domId);
-  // add to slots map here (slots don't get to add themselves)
-  slots[domId] = slot;
+function createOutOfPageSlot(adUnitPath, id = null) {
+  const domId = id || `${prefix}${slotIndex}`;
+  slotIndex += 1;
+  const slot = new OutOfPageSlot(adUnitPath, domId);
+  slots.set(domId, slot);
 
   return slot;
 }
@@ -256,12 +36,119 @@ function createSlot(unit, id = null) {
  *
  * @returns {Slot}
  */
-function createLeaderBoard(unit) {
-  return createSlot(unit)
-    .defineSizeMapping([])
+function createSlot(adUnitPath, sizeMap, id = null) {
+  const domId = id || `${prefix}${slotIndex}`;
+  slotIndex += 1;
+
+  const slot = new Slot(adUnitPath, sizeMap, domId);
+  slots.set(domId, slot);
+
+  return slot;
+}
+
+/**
+ * leaderboard slot class
+ * @link https://developers.google.com/doubleclick-gpt/reference#googletag.Slot_defineSizeMapping
+ * @link https://support.google.com/dfp_premium/answer/1100453?hl=en
+ */
+function createLeaderBoardSlot(adUnitPath, sizeMap, id = null) {
+  const domId = id || `${prefix}${slotIndex}`;
+
+  const leaderBoardMapping = googletag.sizeMapping()
+    .addSize([1024, 768], [
+      [728, 90]
+    ])
+    .addSize([0, 0], [
+      [320, 50]
+    ])
+    .build();
+
+  return createSlot(adUnitPath, sizeMap, domId)
+    .defineSizeMapping(leaderBoardMapping)
     // other "leaderboard" stuff
     .setTargeting('blah', 'what');
 }
+
+/**
+ * leaderboard flex slot class
+ * @link https://developers.google.com/doubleclick-gpt/reference#googletag.Slot_defineSizeMapping
+ * @link https://support.google.com/dfp_premium/answer/1100453?hl=en
+ */
+function createLeaderBoardFlexSlot(adUnitPath, sizeMap, id = null) {
+  const domId = id || `${prefix}${slotIndex}`;
+
+  const leaderBoardFlexMapping = googletag.sizeMapping()
+    .addSize([1024, 768], [
+      [728, 90],
+      [970, 66],
+      [1010, 150],
+      [970, 250],
+      [1010, 250]
+    ])
+    .addSize([0, 0], [
+      [300, 250],
+      [320, 50]
+    ])
+    .build();
+
+  return createSlot(adUnitPath, sizeMap, domId)
+    .defineSizeMapping(leaderBoardFlexMapping);
+}
+
+/**
+ * medium rectangle slot class
+ * @link https://developers.google.com/doubleclick-gpt/reference#googletag.Slot_defineSizeMapping
+ * @link https://support.google.com/dfp_premium/answer/1100453?hl=en
+ */
+function createMediumRectangleSlot(adUnitPath, sizeMap, id = null) {
+  const domId = id || `${prefix}${slotIndex}`;
+
+  const mediumRectangleMapping = googletag.sizeMapping()
+    .addSize([0, 0], [
+      [300, 250]
+    ])
+    .build();
+
+  return createSlot(adUnitPath, sizeMap, domId)
+    .defineSizeMapping(mediumRectangleMapping);
+}
+
+/**
+ * medium rectangle flex slot class
+ * @link https://developers.google.com/doubleclick-gpt/reference#googletag.Slot_defineSizeMapping
+ */
+function createMediumRectangleFlexSlot(adUnitPath, sizeMap, id = null) {
+  const domId = id || `${prefix}${slotIndex}`;
+
+    const mediumRectangleFlexMapping = googletag.sizeMapping()
+      .addSize([0, 0], [
+        [300, 250],
+        [300, 600]
+      ])
+      .build();
+
+  return createSlot(adUnitPath, sizeMap, domId)
+    .defineSizeMapping(mediumRectangleFlexMapping);
+}
+
+/**
+ * large rectangle slot class
+ * @link https://developers.google.com/doubleclick-gpt/reference#googletag.Slot_defineSizeMapping
+ * @link https://support.google.com/dfp_premium/answer/1100453?hl=en
+ */
+function createLargeRectangleSlot(adUnitPath, sizeMap, id = null) {
+  const domId = id || `${prefix}${slotIndex}`;
+
+    const largeRectangleMapping = googletag.sizeMapping()
+      .addSize([0, 0], [
+        [336, 280]
+      ])
+      .build();
+
+  return createSlot(adUnitPath, sizeMap, domId)
+    .defineSizeMapping(largeRectangleMapping);
+}
+
 // add above to export, example use:
 /*
 wbgpt.createLeaderBoard('/123/somesite/blah', 'isthisforid?')
@@ -271,105 +158,22 @@ wbgpt.createLeaderBoard('/123/somesite/blah', 'isthisforid?')
  */
 
 // eliminate class, move to function like above
-class LeaderBoardSlot extends Slot {
-  constructor(unit, sizeMap) {
-    super(unit, sizeMap);
+// class LeaderBoardSlot extends Slot {
+//   constructor(unit, sizeMap) {
+//     super(unit, sizeMap);
 
-    const leaderBoardMapping = googletag.sizeMapping()
-      .addSize([1024, 768], [
-        [728, 90]
-      ])
-      .addSize([0, 0], [
-        [320, 50]
-      ])
-      .build();
+//     const leaderBoardMapping = googletag.sizeMapping()
+//       .addSize([1024, 768], [
+//         [728, 90]
+//       ])
+//       .addSize([0, 0], [
+//         [320, 50]
+//       ])
+//       .build();
 
-    this.slot.defineSizeMapping(leaderBoardMapping);
-  }
-}
-
-/**
- * leaderboard flex slot class
- * @link https://developers.google.com/doubleclick-gpt/reference#googletag.Slot_defineSizeMapping
- */
-class LeaderBoardFlexSlot extends Slot {
-  constructor(unit, sizeMap) {
-    super(unit, sizeMap);
-
-    const leaderBoardFlexMapping = googletag.sizeMapping()
-      .addSize([1024, 768], [
-        [728, 90],
-        [970, 66],
-        [1010, 150],
-        [970, 250],
-        [1010, 250]
-      ])
-      .addSize([0, 0], [
-        [300, 250],
-        [320, 50]
-      ])
-      .build();
-
-    this.slot.defineSizeMapping(leaderBoardFlexMapping);
-  }
-}
-
-/**
- * medium rectangle slot class
- * @link https://developers.google.com/doubleclick-gpt/reference#googletag.Slot_defineSizeMapping
- * @link https://support.google.com/dfp_premium/answer/1100453?hl=en
- */
-class MediumRectangleSlot extends Slot {
-  constructor(unit, sizeMap) {
-    super(unit, sizeMap);
-
-    const mediumRectangleMapping = googletag.sizeMapping()
-      .addSize([0, 0], [
-        [300, 250]
-      ])
-      .build();
-
-    this.slot.defineSizeMapping(mediumRectangleMapping);
-  }
-}
-
-/**
- * medium rectangle flex slot class
- * @link https://developers.google.com/doubleclick-gpt/reference#googletag.Slot_defineSizeMapping
- */
-class MediumRectangleFlexSlot extends Slot {
-  constructor(unit, sizeMap) {
-    super(unit, sizeMap);
-
-    const mediumRectangleFlexMapping = googletag.sizeMapping()
-      .addSize([0, 0], [
-        [300, 250],
-        [300, 600]
-      ])
-      .build();
-
-    this.slot.defineSizeMapping(mediumRectangleFlexMapping);
-  }
-}
-
-/**
- * large rectangle slot class
- * @link https://developers.google.com/doubleclick-gpt/reference#googletag.Slot_defineSizeMapping
- * @link https://support.google.com/dfp_premium/answer/1100453?hl=en
- */
-class LargeRectangleSlot extends Slot {
-  constructor(unit, sizeMap) {
-    super(unit, sizeMap);
-
-    const largeRectangleMapping = googletag.sizeMapping()
-      .addSize([0, 0], [
-        [336, 280]
-      ])
-      .build();
-
-    this.slot.defineSizeMapping(largeRectangleMapping);
-  }
-}
+//     this.slot.defineSizeMapping(leaderBoardMapping);
+//   }
+// }
 
 // todo: there are more factories to create... aren't there other well known sizes?
 
@@ -378,7 +182,7 @@ class LargeRectangleSlot extends Slot {
  * @return {Object}
  */
 function getSlots() {
-  return slots;
+  return slots.values();
 }
 
 /**
@@ -386,9 +190,8 @@ function getSlots() {
  * @param {float} index - slot number
  * @return {Object}
  */
-// todo: return the slot object, not gpt.  (add getGptSlot() to "Slot" class)
 function getSlotByIndex(index) {
-  return slots[`${prefix}-${index}`].slot;
+  return slots.get(`${prefix}${index}`);
 }
 
 /**
@@ -396,9 +199,8 @@ function getSlotByIndex(index) {
  * @param {string} id - slot dom id
  * @return {Object}
  */
-// todo: return the slot
 function getSlotById(id) {
-  return slots[id].slot;
+  return slots.get(id);
 }
 
 /**
@@ -406,6 +208,7 @@ function getSlotById(id) {
  */
 function setDisplayProvider(newDisplayProvider) {
   displayProvider = newDisplayProvider;
+  return this;
 }
 
 /**
@@ -414,6 +217,7 @@ function setDisplayProvider(newDisplayProvider) {
  */
 function display(id) {
   displayProvider.display(id);
+  return this;
 }
 
 /**
@@ -422,6 +226,7 @@ function display(id) {
  */
 function displayByIndex(index) {
   displayProvider.display(getSlotByIndex(1).getSlotElementId());
+  return this;
 }
 
 /**
@@ -429,6 +234,7 @@ function displayByIndex(index) {
  */
 function refreshAllSlots() {
   displayProvider.pubads().refresh();
+  return this;
 }
 
 /**
@@ -436,8 +242,8 @@ function refreshAllSlots() {
  * @param {float} index - slot number
  */
 function refreshSlotByIndex(index) {
-  // todo: getSlotByIndex(...).getGptSlot()
-  displayProvider.pubads().refresh([getSlotByIndex(index)]);
+  displayProvider.pubads().refresh([getSlotByIndex(index).getGptSlot()]);
+  return this;
 }
 
 /**
@@ -445,17 +251,18 @@ function refreshSlotByIndex(index) {
  * @param {string} id - slot dom id
  */
 function refreshSlotById(id) {
-  displayProvider.pubads().refresh([getSlotById(id)]);
+  displayProvider.pubads().refresh([getSlotById(id).getGptSlot()]);
+  return this;
 }
 
 export {
-  Slot,
-  OutOfPageSlot,
-  LargeRectangleSlot,
-  LeaderBoardSlot,
-  LeaderBoardFlexSlot,
-  MediumRectangleSlot,
-  MediumRectangleFlexSlot,
+  createSlot,
+  createOutOfPageSlot,
+  createLeaderBoardSlot,
+  createLeaderBoardFlexSlot,
+  createLargeRectangleSlot,
+  createMediumRectangleSlot,
+  createMediumRectangleFlexSlot,
   display,
   displayByIndex,
   getSlots,

da34556615fd08569296e1ef1e93a4a02555f8b0
Eric Jacobson <contact@efjacobson.com>
2017-05-25 18:33:31 -0700

    add wbabt and wbppid

---
+++ src/wbabt.js
@@ -0,0 +1,107 @@
+/**
+ * @overview
+ * Generates a random value between 1 and 100 for ab testing in googletag/dfp ad calls.
+ *
+ * This script should be included AFTER googletag is loaded.
+ */
+
+let cookieName;
+let cookieExpires;
+let expiry;
+let cookieDomain;
+
+/**
+ * Generates a random integer between 1 and 100
+ * @returns {int}
+ */
+function randomAbt() {
+  return (Math.floor(Math.random() * 100) + 1);
+}
+
+/**
+ * @param {int} value
+ */
+function toStorage(value) {
+  if (window.localStorage) {
+    window.localStorage[cookieName] = value;
+  }
+
+  if (navigator.cookieEnabled) {
+    document.cookie = [cookieName, '=', value, '; expires=', expiry.toUTCString(), '; path=/; domain=', cookieDomain].join('');
+  }
+
+  return value;
+}
+
+/**
+ * @param {int} value
+ */
+function set(value) {
+  return toStorage(value);
+}
+
+/**
+ * @returns {int}
+ */
+function generate() {
+  return set(randomAbt());
+}
+
+/**
+ * @returns {string} results
+ *
+ * @returns {string|boolean}
+ */
+function fromStorage() {
+  let m;
+  let results;
+
+  if (window.localStorage) {
+    results = window.localStorage[cookieName] || false;
+  }
+
+  if (!results && navigator.cookieEnabled) {
+    // eslint-disable-next-line prefer-template
+    m = document.cookie.match(cookieName + '=([^;]*)');
+    results = m && (encodeURIComponent(m[1]) || false);
+  }
+
+  return results || false;
+}
+
+/**
+ * @returns {string}
+ */
+function get() {
+  return (fromStorage() || generate()).toString();
+}
+
+/**
+ * sets the cookie domain
+ */
+function setCookieDomain() {
+  cookieDomain = (document.domain).match(/(.\.)?(\w+\.\w+)$/)[2];
+}
+
+/**
+ * @param {string} newCookieName
+ */
+function setCookieName(newCookieName) {
+  cookieName = newCookieName;
+}
+
+/**
+ * @param {float} newCookieExpires
+ */
+function setCookieExpires(newCookieExpires) {
+  cookieExpires = newCookieExpires;
+  expiry = new Date();
+  expiry.setDate(expiry.getDate() + cookieExpires);
+}
+
+export {
+  get,
+  setCookieDomain,
+  setCookieExpires,
+  setCookieName,
+};

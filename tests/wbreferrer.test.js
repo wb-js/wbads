db85a062bb7e7b32e5656ea522881a33c7af7706
jvasquez75 <jason_vasquez@yahoo.com>
2017-11-20 17:09:41 -0800

    RE: #1 - fix lint errors

--- tests/wbreferrer.test.js
+++ tests/wbreferrer.test.js
@@ -1,64 +1,60 @@
 /* eslint-disable max-len */
 import test from 'tape';
-import { setTokenName, get, getReferrer } from '../src/wbreferrer';
-
-
-
+import { setTokenName, get } from '../src/wbreferrer';
 
 test('wbreferrer - set token', (t) => {
   t.same(setTokenName('wbreferrer'), 'wbreferrer');
   t.end();
 });
 
-
 test('wbreferrer - 3rd party site', (t) => {
+  global.window.location = {
+    hostname: 'www.tmz.com',
+  };
 
-global.window.location = {
-  hostname: 'www.tmz.com',
-};
-global.document = {
-  referrer: 'http://www.google.com',
-}
+  global.document = {
+    referrer: 'http://www.google.com',
+  };
 
-setTokenName('wbreferrer')
+  setTokenName('wbreferrer');
 
-t.equals(get(), 'google.com');
-t.end();
+  t.equals(get(), 'google.com');
+  t.end();
 });
 
 
 test('wbreferrer - local link, with previous referrer', (t) => {
   global.window.sessionStorage = {
-  wbreferrer: '',
-};
-global.window.location = {
-  hostname: 'tmz.com',
-};
-global.document = {
-  referrer: 'http://www.tmz.com',
-}
+    wbreferrer: '',
+  };
 
-setTokenName('wbreferrer')
+  global.window.location = {
+    hostname: 'tmz.com',
+  };
+  global.document = {
+    referrer: 'http://www.tmz.com',
+  };
 
-t.equals(get(), '');
-t.end();
+  setTokenName('wbreferrer');
+
+  t.equals(get(), '');
+  t.end();
 });
 
 
 test('wbreferrer - local link, no previous referrer', (t) => {
   global.window.sessionStorage = {
-  wbreferrer: '',
-};
-global.window.location = {
-  hostname: 'tmz.com',
-};
-global.document = {
-  referrer: 'http://www.tmz.com',
-}
+    wbreferrer: '',
+  };
+  global.window.location = {
+    hostname: 'tmz.com',
+  };
+  global.document = {
+    referrer: 'http://www.tmz.com',
+  };
 
-setTokenName('wbreferrer')
+  setTokenName('wbreferrer');
 
-t.equals(get(), '');
-t.end();
-
+  t.equals(get(), '');
+  t.end();
 });

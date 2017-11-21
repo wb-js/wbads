a461fad4687b28865ffc4ef41897d561dc4647c8
jvasquez75 <jason_vasquez@yahoo.com>
2017-11-20 16:40:46 -0800

    RE: #1 - update unit tests

--- tests/wbreferrer.test.js
+++ tests/wbreferrer.test.js
@@ -1,17 +1,64 @@
 /* eslint-disable max-len */
 import test from 'tape';
-import { setTokenName } from '../src/wbreferrer';
+import { setTokenName, get, getReferrer } from '../src/wbreferrer';
 
-test('wbreferrer tests', (t) => {
-  global.window = {
-    sessionStorage: {},
-  };
-  global.window.location = {
-    hostname: 'www.tmz.com',
-  };
-  global.document = {
-    referrer: 'www.tmz.com',
-  };
+
+
+
+test('wbreferrer - set token', (t) => {
   t.same(setTokenName('wbreferrer'), 'wbreferrer');
   t.end();
+});
+
+
+test('wbreferrer - 3rd party site', (t) => {
+
+global.window.location = {
+  hostname: 'www.tmz.com',
+};
+global.document = {
+  referrer: 'http://www.google.com',
+}
+
+setTokenName('wbreferrer')
+
+t.equals(get(), 'google.com');
+t.end();
+});
+
+
+test('wbreferrer - local link, with previous referrer', (t) => {
+  global.window.sessionStorage = {
+  wbreferrer: '',
+};
+global.window.location = {
+  hostname: 'tmz.com',
+};
+global.document = {
+  referrer: 'http://www.tmz.com',
+}
+
+setTokenName('wbreferrer')
+
+t.equals(get(), '');
+t.end();
+});
+
+
+test('wbreferrer - local link, no previous referrer', (t) => {
+  global.window.sessionStorage = {
+  wbreferrer: '',
+};
+global.window.location = {
+  hostname: 'tmz.com',
+};
+global.document = {
+  referrer: 'http://www.tmz.com',
+}
+
+setTokenName('wbreferrer')
+
+t.equals(get(), '');
+t.end();
+
 });
